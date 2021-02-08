561d6460ec9336a929d15e4b3946841a4f74c29d
sruipdev <sergiotto@outlook.com>
2021-02-08 23:11:46 +0100

    sidebar

---
+++ src/components/layouts/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Sidebar = () => {
+  return (
+    <aside>
+      <h1>
+        MERN<span>Tasks</span>
+      </h1>
+      <div className="projects"><h2>Your projects</h2></div>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,18 @@
 import React from "react";
+import Sidebar from "../layouts/Sidebar";
+
 const Projects = () => {
-  return <h1>From projects</h1>;
+  return (
+    <div className="contenedor-app">
+      <Sidebar />
+
+      <div className="main-section">
+        <main>
+          <div className="container-tasks"></div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
